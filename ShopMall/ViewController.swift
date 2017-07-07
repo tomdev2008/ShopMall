56576dec3c08c496f0622cd0cd05684dec762867
longsirHERO <longsirhero@longsirHEROdeiMac.local>
2017-07-07 16:41:35 +0800

    Initial Commit

---
+++ ShopMall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShopMall
+//
+//  Created by longsirHERO on 2017/7/7.
+//  Copyright © 2017年 WingChingYip(https://longsirhero.github.io). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
